302a489844b5c8bdebe4992e1b0a89c1f075675d
Willem Toorenburgh <willemtoorenburgh@gmail.com>
2024-11-25 13:36:40 -0800

    Use modern appimage settings

--- appimage-support.nix
+++ appimage-support.nix
@@ -2,20 +2,24 @@
 
 {
   # Supporting bits for running AppImages
-  # From the wiki - https://nixos.wiki/wiki/Appimage
+  # From the wiki - https://wiki.nixos.org/wiki/Appimage
+  # replacing old guidance from old wiki
 
-  # TODO: check out programs.appimage.* to see if that replaces any of this
-
-  environment.systemPackages = with pkgs; [
-    appimage-run
-  ];
-
-  boot.binfmt.registrations.appimage = {
-    wrapInterpreterInShell = false;
-    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-    recognitionType = "magic";
-    offset = 0;
-    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-    magicOrExtension = ''\x7fELF....AI\x02'';
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
   };
+
+  # environment.systemPackages = with pkgs; [
+  #   appimage-run
+  # ];
+
+  # boot.binfmt.registrations.appimage = {
+  #   wrapInterpreterInShell = false;
+  #   interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+  #   recognitionType = "magic";
+  #   offset = 0;
+  #   mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+  #   magicOrExtension = ''\x7fELF....AI\x02'';
+  # };
 }
